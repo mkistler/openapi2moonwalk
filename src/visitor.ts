a52c0c66a46189a19fcc8e389fac1f5653e6e0b8
Mike Kistler <mkistler@sbcglobal.net>
2022-09-26 15:21:22 -0500

    Basic support for parameterSchema

--- src/visitor.ts
+++ src/visitor.ts
@@ -8,7 +8,6 @@ import {
   License,
   Operation,
   Parameter,
-  Schema,
   SecurityRequirement,
   SecurityScheme,
   Tag,
@@ -16,7 +15,9 @@ import {
   IDefinition,
   Oas30PathItem,
   OasOperation,
+  Oas30Parameter,
   OasPathItem,
+  Oas30Schema,
 } from "@apicurio/data-models";
 import { notEqual } from "assert";
 
@@ -27,24 +28,34 @@ function addExtensions(node: Info, object) {
   }
 }
 
+// Map of OAS path to MoonWalk pathKey.
+// This memoizes the result of the function below.
+const pathKeys = new Map<string, string>();
+
 // Construct a URI template for a path item.
 // This starts with the path key but then adds all query parameters
 function getPathKey(node: OasPathItem): string {
-  const qpsForOp = (op: OasOperation): string[] =>
-    op.parameters?.filter((v) => v.in === "query").map((v) => v.name) || [];
-  const pathOps = [node.get, node.put, node.post, node.patch, node.delete];
-  var queryParams: Set<string> = new Set();
-  for (const op of pathOps.filter((v) => v !== null)) {
-    qpsForOp(op).forEach((name) => queryParams.add(name));
+  const oasPath = node.getPath();
+  if (!pathKeys.has(oasPath)) {
+    const qpsForOp = (op: OasOperation): string[] =>
+      op.parameters?.filter((v) => v.in === "query").map((v) => v.name) || [];
+    const pathOps = [node.get, node.put, node.post, node.patch, node.delete];
+    var queryParams: Set<string> = new Set();
+    for (const op of pathOps.filter((v) => v !== null)) {
+      qpsForOp(op).forEach((name) => queryParams.add(name));
+    }
+    if (queryParams.size === 0) {
+      pathKeys.set(oasPath, oasPath);
+    } else {
+      pathKeys.set(oasPath, `${oasPath}{?${[...queryParams].join(",")}}`);
+    }
   }
-  if (queryParams.size > 0) {
-    return `${node.getPath()}{?${[...queryParams].join(",")}}`;
-  }
-  return node.getPath();
+  return pathKeys.get(oasPath);
 }
 
 export default class MyCustomVisitor implements IVisitor {
   document: object;
+  currentOperation: object;
   visitComponents(node) {
     //throw new Error('Method not implemented.');
   }
@@ -95,21 +106,41 @@ export default class MyCustomVisitor implements IVisitor {
   visitMediaType(node: License) {
     //throw new Error('Method not implemented.');
   }
-  visitOperation(node: Operation) {
+  visitOperation(node: OasOperation) {
     const pathKey = getPathKey(node.parent() as OasPathItem);
     if (!this.document["paths"][pathKey]["requests"]) {
       this.document["paths"][pathKey]["requests"] = {};
     }
-    const requestKey = node.operationId || (node as OasOperation).getMethod();
+    const requestKey = node.operationId || node.getMethod();
     this.document["paths"][pathKey]["requests"][requestKey] = {
-      method: (node as OasOperation).getMethod(),
+      method: node.getMethod(),
     };
+    this.currentOperation =
+      this.document["paths"][pathKey]["requests"][requestKey];
     //throw new Error('Method not implemented.');
   }
   visitParameterDefinition(node: IDefinition) {
     //throw new Error('Method not implemented.');
   }
-  visitParameter(node: Parameter) {
+  visitParameter(node: Oas30Parameter) {
+    if (!["path", "query"].includes(node.in)) {
+      // We currently only handle path and query parameters
+      return;
+    }
+    if (this.currentOperation) {
+      if (!this.currentOperation["parameterSchema"]) {
+        this.currentOperation["parameterSchema"] = {
+          type: "object",
+          properties: {},
+        };
+      }
+    }
+    const oasSchema = node.schema as Oas30Schema;
+    this.currentOperation["parameterSchema"]["properties"][node.name] = {
+      type: oasSchema.type || undefined,
+      format: oasSchema.format || undefined,
+      description: oasSchema.description || undefined,
+    };
     //throw new Error('Method not implemented.');
   }
   visitPaths(node) {
@@ -121,14 +152,16 @@ export default class MyCustomVisitor implements IVisitor {
       summary: node.summary || undefined,
       description: node.description || undefined,
     };
+    this.currentOperation = undefined;
   }
   visitPropertySchema(node) {
     //throw new Error('Method not implemented.');
   }
+  visitRequestBody(node) {}
   visitSchemaDefinition(node: IDefinition) {
     //throw new Error('Method not implemented.');
   }
-  visitSchema(node: Schema) {
+  visitSchema(node: Oas30Schema) {
     //throw new Error('Method not implemented.');
   }
   visitSecurityRequirement(node: SecurityRequirement) {
